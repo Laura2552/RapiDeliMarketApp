f6ac0c0fcef699b1b35a70ebb5f7cff8dab63126
Laura Lajara <Lajaralaura@outlook.es>
2022-08-03 15:18:06 -0400

    Combinacion avances Karla y Laura

---
+++ lib/src/features/presentation/filter_page/View/customsWidgets/list_tile_checked.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:rapideli_market_app/src/colors/colors.dart';
+import 'package:rapideli_market_app/src/features/presentation/commons_widgets/header_text.dart';
+
+class ListTileCheck extends StatefulWidget {
+  String texto;
+  bool isActive;
+  Function func;
+
+  ListTileCheck({Key key, this.texto, this.isActive, this.func})
+      : super(key: key);
+
+  @override
+  _ListTileCheckState createState() => _ListTileCheckState();
+}
+
+class _ListTileCheckState extends State<ListTileCheck> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      ListTiles(
+          context: context,
+          texto: widget.texto,
+          isActive: widget.isActive,
+          func: widget.func)
+    ]);
+  }
+}
+
+Widget ListTiles(
+    {BuildContext context, texto: String, isActive: bool, Function func}) {
+  return Container(
+      decoration: BoxDecoration(
+          border: Border(
+              bottom: BorderSide(color: Theme.of(context).dividerColor))),
+      child: ListTile(
+        onTap: func,
+        title: headerText(
+            texto: texto,
+            color: isActive ? orange : Colors.black,
+            fontWeight: FontWeight.w300,
+            fontSize: 17),
+        trailing: isActive
+            ? Icon(
+                Icons.check,
+                color: isActive ? orange : gris,
+              )
+            : Text(''),
+      ));
+}
--- lib/src/features/presentation/filter_page/View/filter_page.dart
+++ lib/src/features/presentation/filter_page/View/filter_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rapideli_market_app/src/colors/colors.dart';
 import 'package:rapideli_market_app/src/features/presentation/commons_widgets/header_text.dart';
 import 'package:rapideli_market_app/src/features/presentation/filter_page/View/customsWidgets/categories_filters.dart';
+import 'package:rapideli_market_app/src/features/presentation/filter_page/View/customsWidgets/list_tile_checked.dart';
 
 class FilterPage extends StatefulWidget {
   FilterPage({Key key}) : super(key: key);
@@ -11,6 +12,15 @@ class FilterPage extends StatefulWidget {
 }
 
 class FilterPageState extends State<FilterPage> {
+//Sort by
+  bool mostpopular = false;
+  bool toprated = false;
+  bool pickedforyou = false;
+//Filters
+  bool deals= false;
+  bool vegetarians= false;
+  bool coupon= false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,21 +31,104 @@ class FilterPageState extends State<FilterPage> {
                 [
                   Container(
                     alignment: Alignment.centerLeft,
-                    margin: EdgeInsets.only(top: 15.0, bottom: 15.0, left: 15.0),
+                    margin: EdgeInsets.only(
+                        top: 15.0, bottom: 15.0, left: 15.0),
                     child: headerText(
-                      texto: 'CATEGORIAS',
-                      color: gris,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 17.0
+                        texto: 'CATEGORIAS',
+                        color: gris,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 17.0
                     ),
                   ),
                   Container(
                       padding: EdgeInsets.symmetric(horizontal: 15.0),
-                      child: CategoriesFilter())
+                      child: CategoriesFilter()),
+                  Container(
+                      alignment: Alignment.centerLeft,
+                      margin: EdgeInsets.only(
+                          top: 15.0, bottom: 15.0, left: 15.0),
+                      child: headerText(
+                          texto: 'ORDENAR POR',
+                          color: gris,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 17.0)
+                  ),
+                  _sortByContainer(),
+                  Container(
+                      alignment: Alignment.centerLeft,
+                      margin: EdgeInsets.only(
+                          top: 15.0, bottom: 15.0, left: 15.0),
+                      child: headerText(
+                          texto: 'Filtros',
+                          color: gris,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 17.0)
+                  ),
+                  _filterContainer(),
                 ]
             ))
       ]),
     );
+  }
+
+  Widget _sortByContainer() {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 15.0),
+        child: Column(
+          children: [
+            ListTileCheck(
+              texto: 'Más calificados',
+              isActive: toprated,
+              func: () {
+                setState(() => toprated = !toprated);
+              },
+            ),
+            ListTileCheck(
+              texto: 'Más populares',
+              isActive: mostpopular,
+              func: () {
+                setState(() => mostpopular = !mostpopular);
+              },
+            ),
+            ListTileCheck(
+              texto: 'Elegidos para ti',
+              isActive: pickedforyou,
+              func: () {
+                setState(() => pickedforyou = !pickedforyou);
+              },
+            ),
+          ],
+        ));
+  }
+
+  Widget _filterContainer() {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 15.0),
+        child: Column(
+          children: [
+            ListTileCheck(
+              texto: 'Ofertas',
+              isActive: deals,
+              func: () {
+                setState(() => deals = !deals);
+              },
+            ),
+            ListTileCheck(
+              texto: 'Acepta cupones',
+              isActive: coupon,
+              func: () {
+                setState(() => coupon = !coupon);
+              },
+            ),
+            ListTileCheck(
+              texto: 'Vegetarianos',
+              isActive: vegetarians,
+              func: () {
+                setState(() => vegetarians = !vegetarians);
+              },
+            ),
+          ],
+        ));
   }
 }
 
